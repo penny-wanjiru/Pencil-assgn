b7c3110fd8c0031b48b470db9b27785326e47fac
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-29 00:38:11 +0300

    Create user class

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,18 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-  apiKey: "AIzaSyAhYMYVyJpTjndiHuDkbUzORU0gkw3WSZY",
-  authDomain: "pencil-3fd48.firebaseapp.com",
-  databaseURL: "https://pencil-3fd48.firebaseio.com",
-  projectId: "pencil-3fd48",
-  storageBucket: "pencil-3fd48.appspot.com",
-  messagingSenderId: "896452436176",
-  appId: "1:896452436176:web:452b21fe15104dd579fed5",
-  measurementId: "G-C1R2Z17L6Q"
-};
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAhYMYVyJpTjndiHuDkbUzORU0gkw3WSZY",
+    authDomain: "pencil-3fd48.firebaseapp.com",
+    databaseURL: "https://pencil-3fd48.firebaseio.com",
+    projectId: "pencil-3fd48",
+    storageBucket: "pencil-3fd48.appspot.com",
+    messagingSenderId: "896452436176",
+    appId: "1:896452436176:web:452b21fe15104dd579fed5",
+    measurementId: "G-C1R2Z17L6Q"
+  }
 };
 
 
