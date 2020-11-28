13829505810ae97f7796a24044df3f6220ad0300
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-29 00:13:44 +0300

    Generate component login and editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
