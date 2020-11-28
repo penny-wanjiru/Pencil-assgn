0e1cb265f43855d94afa37d19b980667cd7c2565
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-29 00:23:17 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LogInComponent } from './log-in/log-in.component'; // Add this
+import { EditorComponent } from './editor/editor.component'; // Add this
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LogInComponent },              // Add this
+  { path: 'editor', component: EditorComponent }           // Add this
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
