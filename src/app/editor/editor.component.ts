546a606fcdacf7ae78a2e57635aa8405e7d8fe78
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-29 16:37:02 +0300

    Add medium editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,18 +1,15 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnInit } from '@angular/core';
 declare const MediumEditor: any;
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
-export class EditorComponent implements OnInit {
-  editor: any;
+export class EditorComponent implements AfterViewInit {
+editor: any;
   @ViewChild('editable', { static: true }) editable: ElementRef;
-
-  constructor() { }
-
-  ngAfterViewInit(): void {
+constructor() { }
+ngAfterViewInit(): void {
     this.editor = new MediumEditor(this.editable.nativeElement);
   }
-
 }
