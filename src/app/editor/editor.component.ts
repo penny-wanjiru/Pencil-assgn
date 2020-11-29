66f15e8d78d3a8c33372ea0112ae25827cafbbcb
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-29 12:26:30 +0300

    create editor component

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+declare const MediumEditor: any;
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements OnInit {
+  editor: any;
+  @ViewChild('editable', { static: true }) editable: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.editor = new MediumEditor(this.editable.nativeElement);
   }
 
 }
