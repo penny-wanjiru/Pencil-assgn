a2354b96b7be488d7f9672b0e873ad4e9755c997
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-30 16:01:06 +0300

    Refactor imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,34 @@ import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { EditorComponent } from './editor/editor.component';
 
-/* Firebase services */
-import { AngularFireModule } from "@angular/fire";
+//  Firebase
+import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+
+/* Firebase services */
+import { FirestoreService } from 'src/app/shared/firestore.service';
+import { AuthService } from 'src/app/shared/auth.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
-    EditorComponent
+    EditorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireAuthModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [
+    FirestoreService,
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
