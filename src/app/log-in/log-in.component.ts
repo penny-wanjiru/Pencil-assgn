8d1d1bdd4bfc4eafb0942fc937458c74bb580ac5
Penina Wanjiru <penina.wanjiru@andela.com>
2020-11-30 15:59:34 +0300

    Add providers

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from 'src/app/shared/auth.service';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  styleUrls: ['./log-in.component.scss'],
+  providers: [AuthService]
 })
 export class LogInComponent implements OnInit {
 
